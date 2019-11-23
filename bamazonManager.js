bdfaaaf7d88e95e0abdce161521a142341638dff
Felix Fernandez <fernandezfelix460.gmail.com>
2019-11-23 09:43:06 -0500

    added prompts for manager regarding items to update

--- bamazonManager.js
+++ bamazonManager.js
@@ -12,46 +12,46 @@ var connection = mysql.createConnection({
 });
 connection.connect(function (err) {
     if (err) throw err;
-    
-   
+
+
     managerChoice();
 
 });
 
 function managerChoice() {
-	inquirer.prompt([
-	{
-		type: 'list',
-		name: 'choices',
-		message: 'What would you like to do?',
-		choices: [
-			"View Products for Sale",
-			"View Low Inventory",
-			"Add to Inventory",
-			"Add New Product"
-		]
-	}
-	]).then(function(answer) {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+                "View Products for Sale",
+                "View Low Inventory",
+                "Add to Inventory",
+                "Add New Product"
+            ]
+        }
+    ]).then(function (answer) {
 
-		
-		switch (answer.choices) {
-		    case "View Products for Sale":
-		    	productsForSale();
-		      	break;
 
-		    case "View Low Inventory":
-		    	viewLowInventory();
-		      	break;
+        switch (answer.choices) {
+            case "View Products for Sale":
+                productsForSale();
+                break;
 
-		    case "Add to Inventory":
-		    	addInventory();
-		      	break;
+            case "View Low Inventory":
+                viewLowInventory();
+                break;
 
-		    case "Add New Product":
-		    	addProduct();
-		      	break;
-		}
-	});
+            case "Add to Inventory":
+                addInventory();
+                break;
+
+            case "Add New Product":
+                addProduct();
+                break;
+        }
+    });
 };
 
 
@@ -60,7 +60,7 @@ function productsForSale() {
     connection.query(sqlQuery, function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-            
+
             var table = new Table({
                 head: ["Item Id", "Product Name", "Price", "Items Left"],
                 colWidths: [15, 15, 15, 15],
@@ -74,21 +74,23 @@ function productsForSale() {
         console.log(table.toString());
         viewLowInventory();
     });
- };
+};
 
- function viewLowInventory() {
+function viewLowInventory() {
     var sqlQuery = "SELECT item_id, product_name, stock_quantity FROM products WHERE stock_quantity < 20";
-    connection.query(sqlQuery, function(err, res){
-        if(err) throw err;
-        
-        for(var i = 0; i < res.length; i++){
-            console.log("Item Id number: " + res[i].item_id +  "||" + res[i].product_name);
+    connection.query(sqlQuery, function (err, res) {
+        if (err) throw err;
+
+        console.log("~~~~~~~~~~~~~~~~~~~~~ Low Inventory ~~~~~~~~~~~~~~~~~~~~~");
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item Id number: " + res[i].item_id + "||" + res[i].product_name);
+
         }
         addInventory();
     })
- }
- 
- function addInventory() {
+}
+
+function addInventory() {
     inquirer.prompt([
         {
             type: "input",
@@ -103,9 +105,33 @@ function productsForSale() {
             filter: Number
         }
 
-        ])
+    ]).then(function (answers) {
+            
+            var itemAdd = answers.item
+            var unitsAdded = answers.amount
+        
+                console.log("You have choosen item number: " + itemAdd + " To be increased by: " + unitsAdded);
+                inquirer.prompt([
+                    {
+                    type: "confirm",
+                    name: "confirmation",
+                    message: "Are you sure you would like to proceed?",
+                    
+                }
 
- }
+                ]).then(function (answers){
+                    var response;
+                    if(response === answers.true ){
+                      
+                      
+                    }else{
+                        connection.end();
+                    }
+
+                }) 
+
+        })
+
+}
 
 
- 
